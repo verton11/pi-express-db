ca2f1cef5e46c2ae223b4b1a73f5781def4eb115
verton11 <verton245@gmail.com>
2024-06-25 17:21:33 -0300

    hora e data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const now = new Date()
+  const data = {
+    title: 'Express',
+    name: 'Verton',
+    date: [now.getDate(), now.getMonth(), now.getFullYear(), ].join(' \\ '),
+    hora: now.getHours() + ':' + now.getMinutes()
+  }
+
+  res.render('index', data)
 });
 
 module.exports = router;
